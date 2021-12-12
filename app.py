e4187ff95bf86a2c073536997e8fd8c32a22b659
Dame <adhamsiwi.as@gmail.com>
2021-12-12 17:25:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template,url_for,request
 from joblib import load
+import os
 
 app = Flask(__name__)
 
@@ -20,5 +21,5 @@ def predict():
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
-	port = int(os.environ.get('PORT', 33507))
+	port = int(os.environ.get("PORT", 5000))
+	app.run(host='0.0.0.0', port=port)
